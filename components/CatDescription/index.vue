60f974e64dab89ff3083a991344b752d27ea5939
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-06-05 12:46:24 -0300

    first commit

--- components/CatDescription/index.vue
+++ components/CatDescription/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="m-0 text-center max-w-36 py-12">
-    <h1 class="text-cadet">Cat fact</h1>
+    <h1 class="text-cadet text-4xl mb-10 font-semibold">Cat fact</h1>
     <h2 class="text-violet md:text-xl">{{ description }}</h2>
   </div>
 </template>
